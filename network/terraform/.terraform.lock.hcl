f273d0fee3d697a08519ff33d82f99684efe2241
anfilippov7 <sash.f@rambler.ru>
2024-04-30 13:39:03 +0300

    network1

---
+++ network/terraform/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.117.0"
+  hashes = [
+    "h1:NSHi9jP5RJD4FmQSP9j6D+WXpF23u9fB2UkxR4NnbWE=",
+  ]
+}
---
+++ network/terraform/variables.tf
@@ -0,0 +1,55 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = ""
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "b1gfboajr02kesda5lgl"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "b1g7kr9i41eoi2fqj52o"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+
+
+
+
+
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "centos-7-base" {
+  default = "fd88mqi0d02c7jbkest0"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
